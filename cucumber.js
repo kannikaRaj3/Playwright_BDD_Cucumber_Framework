719288b83527405a00e7e00395f382d0e2dc3c74
Kannikaraj3 <kannu1838@gmail.com>
2026-06-02 12:26:35 +0530

    Fix cucumber configuration and package scripts to point to correct paths

--- cucumber.js
+++ cucumber.js
@@ -1,9 +1,8 @@
 module.exports = {
   default: [
     '--require-module ts-node/register',
-    '--require src/util/world.ts',
     '--require src/StepDefinitions/**/*.ts',
-    '--require src/util/hooks.ts',
+    '--require src/hooks/**/*.ts',
     '--format progress-bar',
     'src/features/*.feature' 
   ].join(' ')
